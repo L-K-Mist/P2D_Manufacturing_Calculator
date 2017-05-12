12e3e5a50ef511d977abd045f20c0158a1991f91
LKMist <vandenbosch.dylan1@gmail.com>
2017-05-12 17:28:02 +0200

    up to triangular on to trapezoid

--- sketch.js
+++ sketch.js
@@ -4,19 +4,48 @@ var cupsLime = 16;
 var cupsPortland = 16;
 var cupsSand = 16;
 
-var triangularPrism = {
-    base: 27,
-    height: 19,
-    thickness: 8.5,
-    area: function () {
+
+//Trapezoid (US)
+//Trapezium (UK)
+//Area = ½(a+b) × h
+//h = vertical height
+
+
+function trapezoidalPrism(longLength, shortLength, height) {
+    this.longLength = longLength;
+    this.shortLength = shortLength;
+    this.height = height;
+    this.area = function () {
+        var calc = 0.5 * (this.longLength + this.longLength)*this.height;
+        return calc;
+        
+
+    }
+}
+
+
+function triangularPrism(base, height, thickness) {
+    this.base = base;
+    this.height = height;
+    this.thickness = thickness;
+    this.area = function () {
         var calc = this.base * 0.5 * this.height;
         return calc;
-    },
-    volume: function () {
+    };
+
+    this.volume = function () {
         var calc = this.area() * this.thickness;
         return calc;
-    }
-};
+    };
+
+}
+
+//Person.prototype.name = function() {
+//    return this.firstName + " " + this.lastName;
+//};
+
+
+
 
 var calcCanvas;
 var ingredientRatiosGUI;
@@ -26,15 +55,15 @@ function p5Gui_setup() {
     sliderRange(0, 30, 1);
     ingredientRatiosGUI = createGui('Ingredient Ratios');
     ingredientRatiosGUI.addGlobals('coffeeTinsWet', 'cupsClay', 'cupsLime', 'cupsPortland', 'cupsSand');
- 
-    
-//    
-//    sliderRange(1, 300, 5);
-//    volumeInputsGUI = createGui('Volume Inputs');
-//    volumeInputsGUI.addGlobals('base', 'height', 'thickness');
-//    triangularPrism.base = base;
-//    triangularPrism.height = height;
-//    triangularPrism.thickness = thickness;
+
+
+    //    
+    //    sliderRange(1, 300, 5);
+    //    volumeInputsGUI = createGui('Volume Inputs');
+    //    volumeInputsGUI.addGlobals('base', 'height', 'thickness');
+    //    triangularPrism.base = base;
+    //    triangularPrism.height = height;
+    //    triangularPrism.thickness = thickness;
 
 
     noLoop();
@@ -45,52 +74,73 @@ var outputPanel;
 var volumeString;
 var areaString;
 
-    var areaText;
-    var volumeText;
+var areaText;
+var volumeText;
+
+var prism;
+
+function setInputVariables() {
 
 
-function setInputVariables(){
-    triangularPrism.base = inputPanel.getRangeValue('Triangle Base');
-    console.log(triangularPrism.base + inputPanel.getRangeValue('Triangle Base'));
-    
-    triangularPrism.height = inputPanel.getRangeValue('Triangle Height');
-    
-    triangularPrism.thickness = inputPanel.getRangeValue('Triangle Thickness');
-    
 
-    
+
+
+}
+
+function upateCalcs() {
+    var base = inputPanel.getRangeValue('Triangle Base');
+
+
+    var height = inputPanel.getRangeValue('Triangle Height');
+
+    var thickness = inputPanel.getRangeValue('Triangle Thickness');
+
+    prism = new triangularPrism(base, height, thickness);
+    //    prism = new triangularPrism(inputPanel.getRangeValue('Triangle Base'), inputPanel.getRangeValue('Triangle Height'), inputPanel.getRangeValue('Triangle Thickness'));
+
+    console.log(prism.area());
+    console.log(prism.volume());
+
+    areaText = new String(prism.area());
+    volumeText = new String(prism.volume());
+
+
+
+    outputPanel.setText("Area is:", areaText);
+    outputPanel.setText("Volume is:", volumeText);
+
 }
 
 function quickSettings_setup() {
-    
-           areaText = triangularPrism.area();
-    volumeText = triangularPrism.volume();
+
+
     //Volume Calculation Inputs
     // var base, height, thickness;
 
-    inputPanel = QuickSettings.create(300, 100);
+    inputPanel = QuickSettings.create(300, 100, 'Triangle Attributes');
+    inputPanel.addHTML('Tips', 'For fine tuning, nudge the active slider with your keyboard left and right arrows');
     inputPanel.addRange('Triangle Base', 5, 400, 1, setInputVariables);
     inputPanel.addRange('Triangle Height', 5, 400, 1, setInputVariables);
     inputPanel.addRange('Triangle Thickness', 5, 400, 1, setInputVariables);
-    
- 
-    
-    
-    outputPanel = QuickSettings.create(300, 300, 'Volume Outputs');
+    inputPanel.addButton('Calculate', upateCalcs);
 
+    outputPanel = QuickSettings.create(300, 600, 'Volume Outputs');
     outputPanel.addText("Area is:");
-    outputPanel.setText("Area is:", areaText);
+
 
     outputPanel.addText("Volume is:");
-    outputPanel.setText("Volume is:", volumeText);
-    
+
+
 }
 
 function setup() {
+
+    var testing = new triangularPrism(20, 30, 40);
+    console.log(testing.area());
     //calcCanvas = createCanvas(windowWidth / 4, windowHeight);
     p5Gui_setup();
     quickSettings_setup();
-
+    upateCalcs();
 }
 
 //alert("The area of the triangle is: " +  triangularPrism.area() + " millimeters cubed.");
